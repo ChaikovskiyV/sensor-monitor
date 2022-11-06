475c5673b57a2eef11c5cc0d481e9657b9818d42
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-11-07 01:29:52 +0300

    RequestParamNames with parameters name constants init.

---
+++ src/main/java/com/example/sensormonitor/model/service/RequestParamNames.java
@@ -0,0 +1,10 @@
+package com.example.sensormonitor.model.service;
+
+public final class RequestParamNames {
+    public static final String SEARCH = "search";
+    public static final String PAGE = "page";
+    public static final String LIMIT = "limit";
+
+    private RequestParamNames() {
+    }
+}
